2b38499e1342037565f1cdc63b59cf694a42f097
gurayasu <86140172+gurayasu@users.noreply.github.com>
2021-10-14 23:30:28 +0900

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+use App\Book;
+use Illuminate\Http\Request;
+
+/**
+* 本の一覧表示(books.blade.php)
+*/
+Route::get('/', function () {
+    return view('books');
+});
+
+/**
+* 本を追加 
+*/
+Route::post('/books', function (Request $request) {
+    //
+});
+
+/**
+* 本を削除 
+*/
+Route::delete('/book/{book}', function (Book $book) {
+    //
+});
